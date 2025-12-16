4af3dcc2691fb560ee020e45507ac3b4af46b3ff
Raghav Baheti (k23098128) <raghav.baheti@kcl.ac.uk>
2025-12-16 12:29:25 +0000

    prompt_manager changes

--- Prompts/prompt_manager.py
+++ Prompts/prompt_manager.py
@@ -2,6 +2,10 @@ from dataclasses import dataclass
 import yaml
 from typing import Dict
 from os import path
+
+class PromptNotLoaded(Exception):
+    def __init__(self,message):
+        super().__init__("prompt not loaded use method load_prompt() first")
 @dataclass
 class Prompt:
     """class for native prompts"""
@@ -11,14 +15,22 @@ class Prompt:
 class Prompt_Manager():
     def __init__(self,base_dir:str):
         self.base_dir : str = base_dir
-        self.prompt_dict : Dict[str,str] = None
+        self.current_prompt_dict : Dict[str,str] = None
+        self.current_prompt : Prompt = None 
     
     def load_prompt(self,prompt:Prompt):
-        self.current_prompt_template = prompt
+        self.current_prompt = prompt
+        print(path.join(self.base_dir,prompt.prompt_name,prompt.prompt_version + '.yaml'))
         with open(path.join(self.base_dir,prompt.prompt_name,prompt.prompt_version + '.yaml')) as f:
             self.prompt_dict = yaml.safe_load(f)
+        print(self.current_prompt_dict)
     
     def build_prompt_message(self,insertions: Dict[str,str]):
+        if not self.current_prompt:
+            raise PromptNotLoaded
+        
+        
+
         
 
 
